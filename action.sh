28e7559fc7736d50f5828fac4b4662c185364499
José Ángel Martín Baos <joseangelmartinb@gmail.com>
2020-12-08 14:12:51 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -41,7 +41,7 @@ echo "Using '${URL}' to download Hugo"
 curl -sSL "${URL}" > /tmp/hugo.deb && dpkg --force architecture -i /tmp/hugo.deb
 
 # Install Hugo Go dependency
-sudo snap install --classic go
+snap install --classic go
 
 echo "Building the Hugo site with: 'hugo ${HUGO_ARGS}'"
 hugo "${HUGO_ARGS}"
